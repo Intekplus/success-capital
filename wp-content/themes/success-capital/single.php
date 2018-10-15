1c40ee65acb5c9f6adf1b211097fac939702163e
Master Jay <j.bergeron@intekplus.ca>
2018-10-14 22:44:28 -0400

    single page builder done

--- wp-content/themes/success-capital/single.php
+++ wp-content/themes/success-capital/single.php
@@ -9,9 +9,21 @@ if (have_posts()) {
 
 <div class="container-fluid">
     <div class="container--main">
-        <?php the_content() ?>
+        <div class="preparer-sa-retraite" style="background-image: url('<?= get_the_post_thumbnail_url() ?>')">
+            <div class="retraite-text">
+                <?php the_title() ?>
+            </div>
+        </div>
+        <div class="shadow-container container-fluid">
+            <div class="content-box container--main">
+                <div class="box">
+                    <?php get_news_build() ?>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
+
 
 <?php
 
---
+++ wp-content/themes/success-capital/views/containers/news/twitter.php
@@ -0,0 +1,3 @@
+<div class="twitter-citation">
+  <?php the_sub_field('citation'); ?>
+</div>
